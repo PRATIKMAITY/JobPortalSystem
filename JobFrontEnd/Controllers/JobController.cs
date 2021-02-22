4629eb99b76f4f8499054f0b4b793b20232d4d4f
PRATIK MAITY <pratikmaity1993@gmail.com>
2021-02-23 00:05:47 +0530

    final

--- JobFrontEnd/Controllers/JobController.cs
+++ JobFrontEnd/Controllers/JobController.cs
@@ -19,12 +19,10 @@ namespace JobFrontEnd.Controllers
         // GET: All Job
         public async Task<ActionResult> Index(int? page)
         {
-<<<<<<< HEAD
+
             int pageNumber = (page ?? 1);
             int pagesize = 6;
-=======
-            
->>>>>>> 8a2235c567bd3e247cce572f0a9c3db2ac8dbe64
+
             string loggedin;
             try
             {
@@ -40,7 +38,7 @@ namespace JobFrontEnd.Controllers
             }
 
             if (loggedin == "1")
-<<<<<<< HEAD
+
 
             {
                 ViewBag.loginflagmvc = 1;
@@ -59,22 +57,12 @@ namespace JobFrontEnd.Controllers
 
                 var finallist = AllJobs.Skip(excluderecord).Take(pagesize).ToList();
                 model.AllJobs = finallist;
-=======
-            
-                {
-                    ViewBag.loginflagmvc = 1;
-                }
-                var model = new ListAllJobViewModel();
-                var streamTask = await client.GetAsync(BaseUrl + "Job/GetAllJob");
-                if (streamTask.IsSuccessStatusCode)
-                {
-                    var response = streamTask.Content.ReadAsStringAsync().Result;
-                    model.AllJobs = JsonConvert.DeserializeObject<List<JobViewModel>>(response);
-                  
-                }
+            } 
+                
                 return View(model);
->>>>>>> 8a2235c567bd3e247cce572f0a9c3db2ac8dbe64
-            }
+
+            
+        }
         
         public async Task<ActionResult> Details(string jobid)
         {
@@ -108,37 +96,6 @@ namespace JobFrontEnd.Controllers
 
             return View(model);
         }
-        public async Task<ActionResult> Details1(string jobloc)
-        {
-            object loggedin;
-
-            try
-            {
-                loggedin = Request.Cookies["LogInFlag"].Value;
-
-            }
-            catch (System.NullReferenceException ex)
-            {
-                // not logged in or null id.
-                loggedin = "0";
-            }
-            if (loggedin.ToString() == "1")
-            {
-                ViewBag.loginflagmvc = 1;
-            }
-
-
-            var model = new JobViewModel();
-            var streamTask = await client.GetAsync(BaseUrl + "Job/GetJobByLocation?loc=" + jobloc);
-            if (streamTask.IsSuccessStatusCode)
-            {
-                var response = streamTask.Content.ReadAsStringAsync().Result;
-                model = JsonConvert.DeserializeObject<JobViewModel>(response);
-            }
-
-
-            return View(model);
-        }
-
+        
     }
 }
